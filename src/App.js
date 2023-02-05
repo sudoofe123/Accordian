1ebf6b5e9a8d941a7d778da2086ef96a093ca407
Sudhanshu kumar <sudhanshukumar9211@gmail.com>
2023-02-05 23:01:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "./styles.css";
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello CodeSandbox</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//     </div>
+//   );
+// }
+
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
+function App() {
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
+
+// import React, { useState } from 'react';
+// import data from './data';
+// import SingleQuestion from './Question';
+
+// function App(){
+
+//   const [question,setQuestions]=useState(data);
+
+//   return
+//   <main>
+
+//   </main>
+// }
